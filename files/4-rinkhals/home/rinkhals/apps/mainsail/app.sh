3fa02fa74931f6c067f5e88635588ac7566a1950
Julien Batonnet <julien.batonnet@gmail.com>
2025-02-24 00:11:43 -0500

    Basic lighttpd for mainsail

---
+++ files/4-rinkhals/home/rinkhals/apps/mainsail/app.sh
@@ -0,0 +1,36 @@
+source /useremain/rinkhals/.current/tools.sh
+
+APP_ROOT=$(dirname $(realpath $0))
+
+status() {
+    PIDS=$(get_by_name rinkhals-monitor.sh)
+
+    if [ "$PIDS" == "" ]; then
+        report_status $APP_STATUS_STOPPED
+    else
+        report_status $APP_STATUS_STARTED
+    fi
+}
+start() {
+    lighttpd -f $APP_ROOT/lighttpd.conf &
+    PID=$!
+}
+stop() {
+    kill_by_name rinkhals-monitor
+}
+
+case "$1" in
+    status)
+        status
+        ;;
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    *)
+        echo "Usage: $0 {status|start|stop}" >&2
+        exit 1
+        ;;
+esac
