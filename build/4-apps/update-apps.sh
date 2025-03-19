82113bba1eabdc26268297baf1aade352cd5b963
Julien Batonnet <julien.batonnet@gmail.com>
2025-03-19 18:47:20 -0400

    Updated Mainsail and Fluidd apps to last release

--- build/4-apps/update-apps.sh
+++ build/4-apps/update-apps.sh
@@ -10,22 +10,22 @@ cd /work
 # Mainsail
 echo "Downloading Mainsail..."
 
-wget -O mainsail.zip https://github.com/mainsail-crew/mainsail/releases/download/v2.13.1/mainsail.zip
+wget -O mainsail.zip https://github.com/mainsail-crew/mainsail/releases/download/v2.13.2/mainsail.zip
 unzip -d mainsail mainsail.zip
 
-mkdir -p /files/4-rinkhals/home/rinkhals/apps/mainsail/mainsail
-rm -rf /files/4-rinkhals/home/rinkhals/apps/mainsail/mainsail/*
+mkdir -p /files/4-rinkhals/home/rinkhals/apps/25-mainsail/mainsail
+rm -rf /files/4-rinkhals/home/rinkhals/apps/25-mainsail/mainsail/*
 cp -pr /work/mainsail/* /files/4-apps/home/rinkhals/apps/25-mainsail/mainsail
 
 
 # Fluidd
 echo "Downloading Fluidd..."
 
-wget -O fluidd.zip https://github.com/fluidd-core/fluidd/releases/download/v1.31.2/fluidd.zip
+wget -O fluidd.zip https://github.com/fluidd-core/fluidd/releases/download/v1.33.0/fluidd.zip
 unzip -d fluidd fluidd.zip
 
-mkdir -p /files/4-rinkhals/home/rinkhals/apps/fluidd/fluidd
-rm -rf /files/4-rinkhals/home/rinkhals/apps/fluidd/fluidd/*
+mkdir -p /files/4-rinkhals/home/rinkhals/apps/26-fluidd/fluidd
+rm -rf /files/4-rinkhals/home/rinkhals/apps/26-fluidd/fluidd/*
 cp -pr /work/fluidd/* /files/4-apps/home/rinkhals/apps/26-fluidd/fluidd
 
 
