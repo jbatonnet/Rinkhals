f717ab797eadffc37f594cd2a704c258d81917d1
Julien Batonnet <julien.batonnet@gmail.com>
2025-03-21 15:02:02 -0400

    Switch to generated Moonraker config as well

--- files/4-apps/home/rinkhals/apps/40-moonraker/moonraker.sh
+++ files/4-apps/home/rinkhals/apps/40-moonraker/moonraker.sh
@@ -4,5 +4,9 @@
 python -m venv --without-pip .
 . bin/activate
 
+# Generate configuration
+[ -f /userdata/app/gk/printer_data/config/moonraker.custom.conf ] || cp moonraker.custom.conf /userdata/app/gk/printer_data/config/moonraker.custom.conf
+python /opt/rinkhals/scripts/process-cfg.py moonraker.conf > /userdata/app/gk/printer_data/config/moonraker.generated.conf
+
 # Start Klippy
 HOME=/userdata/app/gk python ./moonraker/moonraker/moonraker.py -c /userdata/app/gk/printer_data/config/moonraker.generated.conf >> $RINKHALS_ROOT/logs/app-moonraker.log 2>&1
