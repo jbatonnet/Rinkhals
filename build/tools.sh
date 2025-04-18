c77ed55451ebd898c8e3a461c41a132e9b0f22af
Bas van Doren <basvdoren@gmail.com>
2025-04-18 20:48:50 +0200

    Create SWU without redundant compression

--- build/tools.sh
+++ build/tools.sh
@@ -9,7 +9,6 @@ prepare_tgz() {
 
     cd $UPDATE_DIRECTORY
     tar -czf $SWU_DIR/update_swu/setup.tar.gz --exclude='setup.tar.gz' .
-    #tar -cf $SWU_DIR/update_swu/setup.tar --exclude='setup.tar' .
 }
 compress_swu() {
     KOBRA_MODEL_CODE=$1
@@ -22,9 +21,9 @@ compress_swu() {
     cd $SWU_DIR
 
     if [ "$KOBRA_MODEL_CODE" = "K2P" ] || [ "$KOBRA_MODEL_CODE" = "K3" ]; then
-        zip -P U2FsdGVkX19deTfqpXHZnB5GeyQ/dtlbHjkUnwgCi+w= -r $SWU_NAME update_swu
+        zip -0 -P U2FsdGVkX19deTfqpXHZnB5GeyQ/dtlbHjkUnwgCi+w= -r $SWU_NAME update_swu
     elif [ "$KOBRA_MODEL_CODE" = "KS1" ]; then
-        zip -P U2FsdGVkX1+lG6cHmshPLI/LaQr9cZCjA8HZt6Y8qmbB7riY -r $SWU_NAME update_swu
+        zip -0 -P U2FsdGVkX1+lG6cHmshPLI/LaQr9cZCjA8HZt6Y8qmbB7riY -r $SWU_NAME update_swu
     else
         echo "Unknown Kobra model code: $KOBRA_MODEL_CODE"
         exit 1
