389f770d7bad7b8812536ff3eb518e97744faf85
Julien Batonnet <julien.batonnet@gmail.com>
2025-05-30 00:03:17 -0400

    f

--- build/1-buildroot/external/package/rockchip-mpp/rockchip-mpp.mk
+++ build/1-buildroot/external/package/rockchip-mpp/rockchip-mpp.mk
@@ -2,4 +2,7 @@ ROCKCHIP_MPP_VERSION = develop
 ROCKCHIP_MPP_SITE = https://github.com/nyanmisaka/mpp
 ROCKCHIP_MPP_SITE_METHOD = git
 
+ROCKCHIP_MPP_STAGING = YES
+ROCKCHIP_MPP_TARGET = NO
+
 $(eval $(cmake-package))
