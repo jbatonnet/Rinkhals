a003ce80891d169804d7cce225508c84ec9dc178
Julien Batonnet <julien.batonnet@gmail.com>
2025-05-30 00:03:17 -0400

    f

--- build/1-buildroot/external/package/ffmpeg-rockchip/ffmpeg-rockchip.mk
+++ build/1-buildroot/external/package/ffmpeg-rockchip/ffmpeg-rockchip.mk
@@ -2,15 +2,26 @@ FFMPEG_ROCKCHIP_VERSION = 57d5befee96f229b05fa09334a4d7a6f95a324bd
 FFMPEG_ROCKCHIP_SITE = https://github.com/nyanmisaka/ffmpeg-rockchip
 FFMPEG_ROCKCHIP_SITE_METHOD = git
 
-FFMPEG_ROCKCHIP_DEPENDENCIES = rockchip-mpp rockchip-rga
+FFMPEG_ROCKCHIP_DEPENDENCIES = rockchip-mpp rockchip-rga libdrm
+
+define FFMPEG_ROCKCHIP_CONFIGURE_CMDS
+	(cd $(@D); rm -f config.cache; \
+		./configure \
+		--prefix=/usr \
+		--enable-gpl \
+		--enable-version3 \
+		--enable-rkmpp \
+		--enable-rkrga \
+		--disable-x86asm \
+	)
+endef
 
 define FFMPEG_ROCKCHIP_BUILD_CMDS
-	./configure --prefix=/usr --enable-gpl --enable-version3 --enable-libdrm --enable-rkmpp --enable-rkrga
-	make -j $(nproc)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
 endef
 
 define FFMPEG_ROCKCHIP_INSTALL_TARGET_CMDS
-	make install
+	$(INSTALL)
 endef
 
 $(eval $(generic-package))
--- build/1-buildroot/external/package/rockchip-rga/rockchip-rga.mk
+++ build/1-buildroot/external/package/rockchip-rga/rockchip-rga.mk
@@ -2,4 +2,7 @@ ROCKCHIP_RGA_VERSION = linux-rga-multi
 ROCKCHIP_RGA_SITE = https://github.com/nyanmisaka/rk-mirrors
 ROCKCHIP_RGA_SITE_METHOD = git
 
+ROCKCHIP_RGA_STAGING = YES
+ROCKCHIP_RGA_TARGET = NO
+
 $(eval $(meson-package))
