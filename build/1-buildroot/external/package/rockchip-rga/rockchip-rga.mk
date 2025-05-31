5507b092099b98921f580c9aa316ec5a968a2139
Julien Batonnet <julien.batonnet@gmail.com>
2025-05-31 00:03:42 -0400

    f

--- build/1-buildroot/external/package/rockchip-rga/rockchip-rga.mk
+++ build/1-buildroot/external/package/rockchip-rga/rockchip-rga.mk
@@ -2,7 +2,7 @@ ROCKCHIP_RGA_VERSION = linux-rga-multi
 ROCKCHIP_RGA_SITE = https://github.com/nyanmisaka/rk-mirrors
 ROCKCHIP_RGA_SITE_METHOD = git
 
-ROCKCHIP_RGA_STAGING = YES
-ROCKCHIP_RGA_TARGET = YES
+ROCKCHIP_RGA_DEPENDENCIES = host-pkgconf
+ROCKCHIP_RGA_INSTALL_STAGING = YES
 
 $(eval $(meson-package))
