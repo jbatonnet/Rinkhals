23ac3b72afd1194b6ec0701fc3dba1b877826964
m4d53 <89360303+m4d53@users.noreply.github.com>
2025-07-09 08:31:16 +0200

    Added comments
    Added some comments just for the clarity

--- files/4-apps/home/rinkhals/apps/40-moonraker/kobra.py
+++ files/4-apps/home/rinkhals/apps/40-moonraker/kobra.py
@@ -473,9 +473,9 @@ class Kobra:
                             f'M140 S{bed_temp}', # Set bed to 60
                             f'M109 S{extru_temp}', # Wait hotend to 170
                             f'M190 S{bed_temp}', # Wait bed to 60
-                            'WIPE_ENTER',
-                            'WIPE_NOZZLE',
-                            'WIPE_EXIT',
+                            'WIPE_ENTER', # Move to wiping position
+                            'WIPE_NOZZLE', # Wipe nozzle
+                            'WIPE_EXIT', # Exit wiping position
                             f'M109 S{extru_end_temp}', # Wait hotend to 140
                             'BED_MESH_CALIBRATE',
                             'TURN_OFF_HEATERS',
