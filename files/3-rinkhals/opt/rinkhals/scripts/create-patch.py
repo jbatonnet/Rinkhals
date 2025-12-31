ff13cba010c0891dfb7e2edae95212302b3a260b
Julien Batonnet <julien.batonnet@gmail.com>
2025-12-31 18:39:16 -0500

    Fix patch offsets for KS1 and KS1M

--- files/3-rinkhals/opt/rinkhals/scripts/create-patch.py
+++ files/3-rinkhals/opt/rinkhals/scripts/create-patch.py
@@ -178,14 +178,14 @@ def patch_K3SysUi(binaryPath, modelCode, version):
         patchJumpAddress = 0x12b04c
         patchReturnAddress = 0x12b054
     elif modelCode == 'KS1' and version == '2.5.9.9':
-        buttonCallback = k3sysui.symbols['_ZZN10MainWindow26AcSettingGeneralPageUiInitEvENKUlRK11QModelIndexE0_clES2_']
-        patchJumpAddress = 0x14bbc0
-        patchReturnAddress = 0x14bbc8
+        buttonCallback = k3sysui.symbols['_ZZN10MainWindow21AcSettingDeviceUiInitEvENKUlRK11QModelIndexE0_clES2_']
+        patchJumpAddress = 0x14a51c
+        patchReturnAddress = 0x14a524
         
     elif modelCode == 'KS1M' and version == '2.1.6':
-        buttonCallback = k3sysui.symbols['_ZZN10MainWindow26AcSettingGeneralPageUiInitEvENKUlRK11QModelIndexE0_clES2_']
-        patchJumpAddress = 0x14bbb8
-        patchReturnAddress = 0x14bbc0
+        buttonCallback = k3sysui.symbols['_ZZN10MainWindow21AcSettingDeviceUiInitEvENKUlRK11QModelIndexE0_clES2_']
+        patchJumpAddress = 0x14a514
+        patchReturnAddress = 0x14a51c
 
     else:
         raise Exception('Unsupported model and version')
